b59404590c6104fc116c11806c1ba27d07fa97e3
Will <willchesson@gmail.com>
2022-06-21 17:06:07 -0400

    commit tree 0d1b3f6951b54683ffdb3438f8b4409c845a5d1d

---
+++ app/models/show.rb
@@ -0,0 +1,10 @@
+class Show < ActiveRecord::Base
+  def self.highest_rating
+    Show.maximum(:rating)
+  end
+
+  def self.most_popular_show
+    Show.where("rating = ?", self.highest_rating).first
+  end
+
+end
